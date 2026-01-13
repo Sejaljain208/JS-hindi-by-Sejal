b3e04c5caf1c8f60d967eb6c91ffc79eb152328b
Sejal Jain <sejal6249@gmail.com>
2026-01-13 20:05:13 +0530

    Add files via upload

---
+++ 05)DynamicTyping.js
@@ -0,0 +1,16 @@
+//---------------
+
+// dynamic typing : 
+// js is not static typing language. javascript is a dynamically typed language . iska matlab ye hai ki hum variable ko declare karte waqt uska data type nahi batate hai or hum variable ki value ko kabhi bhi change kar sakte hai or uska data type bhi change ho jata hai.
+
+let e = 10; //number 
+e = "sejal"; //string
+e = true; //boolean
+e = null; //null
+e = []; //array
+e = {}; //object
+e = function() {}; //function
+
+//humne variable e ki value ko baar baar change kiya or uska data type bhi change hota gaya. yehi hai dynamic typing ka concept.
+
+//---------------
