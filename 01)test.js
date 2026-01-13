173066f12c10c593014e8dac6f3dd1fa1823f02c
Sejal Jain <sejal6249@gmail.com>
2026-01-13 18:36:52 +0530

    Add files via upload

---
+++ 01)test.js
@@ -0,0 +1,11 @@
+/* difference text file and js file  
+    A text file is a simple file that contains plain text without any special formatting or functionality. It is typically used for storing notes, documentation, or any other type of unstructured information. Text files usually have a .txt extension.      
+    and 
+    in js file we write javascript code which is used to create dynamic and interactive web pages. JavaScript files have a .js extension and can be linked to HTML files to add functionality to web pages. JavaScript is a programming language that allows developers to create complex features such as form validation, animations, and interactive elements on websites.
+ */ 
+// how to run js file: click on run button on top right corner.
+// shortcut to run js file: ctrl + alt + n
+
+
+
+console.log("sejal");
