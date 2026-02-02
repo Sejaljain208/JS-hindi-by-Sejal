fa2f00ae89130f5ec048a8ba8b391bcf96f78969
Sejal Jain <sejal6249@gmail.com>
2026-02-03 01:50:54 +0530

    Add files via upload

---
+++ 17)Array.js
@@ -0,0 +1,35 @@
+// an array is like a row of boxes,where each box can hold a value and has an index number to identify its position in the row.
+
+// array helps you to store multiple values in a single variable instead of creating separate variables for each value.
+
+// creting an array
+let fruits = ['apple', 'banana', 'cherry', 'date'];
+console.log(fruits); // output: ['apple', 'banana', 'cherry', 'date']
+
+// accessing array elements using index
+console.log(fruits[0]); // output: apple
+console.log(fruits[2]); // output: cherry
+
+// modifying array elements
+fruits[1] = 'blueberry';
+console.log(fruits); // output: ['apple', 'blueberry', 'cherry', 'date']
+
+// indexing starts from 0. we can update the value at any index position.any type of value can be stored in an array.
+
+// array methods : there are various methodes available to manipulate arrays. some commonly used methods are: 
+//1. push()
+//2. pop()
+//3. shift()
+//4. unshift()
+//5. splice()
+//6. slice()
+//7. forEach()
+//8. map()
+//9. filter()
+//10. reduce()
+//11. find()
+//12. sort()
+//13. reverse() 
+//14. some()
+//15. every()
+   
