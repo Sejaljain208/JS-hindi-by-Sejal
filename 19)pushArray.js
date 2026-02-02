d1d1b0bb8e5e302078468270f0ae6e2b2971dd55
Sejal Jain <sejal6249@gmail.com>
2026-02-03 01:51:31 +0530

    Add files via upload

---
+++ 19)pushArray.js
@@ -0,0 +1,10 @@
+// push() : last mai element add karta hai array mein
+
+let arr = [10, 20, 30];
+console.log("Original Array:", arr); // output: [10, 20, 30]
+
+arr.push(40); // adds 40 at the end of the array
+console.log("Array after push(40):", arr); // output: [10, 20, 30, 40]
+
+arr.push(50, 60); // adds 50 and 60 at the end of the array
+console.log("Array after push(50, 60):", arr); // output: [10, 20, 30, 40, 50, 60]
