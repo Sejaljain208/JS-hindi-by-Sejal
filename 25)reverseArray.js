eec64fc6e024d5ba530e259a00b03be24754fd5b
Sejal Jain <sejal6249@gmail.com>
2026-02-03 01:53:56 +0530

    Add files via upload

---
+++ 25)reverseArray.js
@@ -0,0 +1,8 @@
+// reverse (): ye array ke elements ko ulta kar deta hai, matlab last element pehle aa jata hai aur pehla element last me chala jata hai.
+
+let arr = [10, 20, 30, 40, 50];
+console.log("Original Array:", arr); // output: [10, 20, 30, 40, 50]
+
+arr.reverse();
+console.log("Reversed Array:", arr); // output: [50, 40, 30, 20, 10]
+// array ke elements ko ulta kar diya gaya hai
