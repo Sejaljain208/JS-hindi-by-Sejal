cb5e954ce16c849a8261b956495878839362e5c0
Sejal Jain <sejal6249@gmail.com>
2026-02-03 01:51:13 +0530

    Add files via upload

---
+++ 18)popArray.js
@@ -0,0 +1,7 @@
+// pop() : last element removal from an array
+
+let arr = [10, 20, 30, 40, 50];
+console.log("Original Array:", arr); // output: [10, 20, 30, 40, 50]    
+
+arr.pop(); // removes the last element (50)
+console.log("Array after pop():", arr); // output: [10, 20, 30, 40]
